faa8e400f4cf044b8560f6567577a1950040103c
HThanh-how <thanhlt1235@gmail.com>
2024-05-26 23:25:18 +0700

    Update số lẻ

--- app/dashboard/component/Stats.tsx
+++ app/dashboard/component/Stats.tsx
@@ -30,12 +30,18 @@ function StatItem(props: StatItemProps) {
   const { Label, Num, background } = props;
 
   // Định dạng Num thành chuỗi với dấu cách sau mỗi ba chữ số
-  const formattedNum = Num ? Num.toLocaleString('en-US', { useGrouping: true }).replace(/,/g, ' ') : "0";
+  const formattedNum = Num
+    ? Num.toLocaleString("en-US", { useGrouping: true }).replace(/,/g, " ")
+    : "0";
 
   return (
     <StatGroup bgGradient={background} p={4} borderRadius={"md"}>
       <Stat>
-        <Flex textColor={'white'} alignItems={"center"} justify={"space-between"}>
+        <Flex
+          textColor={"white"}
+          alignItems={"center"}
+          justify={"space-between"}
+        >
           <Box>
             <StatLabel>{Label}</StatLabel>
             <StatNumber>{formattedNum}</StatNumber>
@@ -57,15 +63,17 @@ function Stats() {
     isSuccess,
     isError,
     error,
-  } = useGetStatistic2Query(1)
+  } = useGetStatistic2Query(1);
 
   const getData = useMemo(() => {
-    if (isSuccess) return data.data
-  }, [data])
+    if (isSuccess) return data.data;
+  }, [data]);
 
-
-
-  const CountDown = (p0: number, p1: number, { initialValue, duration }: { initialValue: number; duration: number; }) => {
+  const CountDown = (
+    p0: number,
+    p1: number,
+    { initialValue, duration }: { initialValue: number; duration: number }
+  ) => {
     const [count, setCount] = useState(initialValue);
 
     useEffect(() => {
@@ -85,7 +93,6 @@ function Stats() {
     return <>{Math.round(count)}</>;
   };
 
-
   return (
     <>
       {isLoading ? (
@@ -95,8 +102,38 @@ function Stats() {
           spacing={{ base: 2, md: 10 }}
           mb="4"
         >
-          <Skeleton rounded={"md"} > <StatItem Label="Đơn mới hôm nay" Num={9999} background="linear-gradient(135deg, #13072e, #3f2182)" /></Skeleton>
-          <Skeleton rounded={"md"} > <StatItem Label="Đơn mới hôm nay" Num={9999} background="linear-gradient(135deg, #13072e, #3f2182)" /></Skeleton><Skeleton rounded={"md"} > <StatItem Label="Đơn mới hôm nay" Num={9999} background="linear-gradient(135deg, #13072e, #3f2182)" /></Skeleton><Skeleton rounded={"md"} > <StatItem Label="Đơn mới hôm nay" Num={9999} background="linear-gradient(135deg, #13072e, #3f2182)" /></Skeleton>
+          <Skeleton rounded={"md"}>
+            {" "}
+            <StatItem
+              Label="Đơn mới hôm nay"
+              Num={9999}
+              background="linear-gradient(135deg, #13072e, #3f2182)"
+            />
+          </Skeleton>
+          <Skeleton rounded={"md"}>
+            {" "}
+            <StatItem
+              Label="Đơn mới hôm nay"
+              Num={9999}
+              background="linear-gradient(135deg, #13072e, #3f2182)"
+            />
+          </Skeleton>
+          <Skeleton rounded={"md"}>
+            {" "}
+            <StatItem
+              Label="Đơn mới hôm nay"
+              Num={9999}
+              background="linear-gradient(135deg, #13072e, #3f2182)"
+            />
+          </Skeleton>
+          <Skeleton rounded={"md"}>
+            {" "}
+            <StatItem
+              Label="Đơn mới hôm nay"
+              Num={9999}
+              background="linear-gradient(135deg, #13072e, #3f2182)"
+            />
+          </Skeleton>
         </SimpleGrid>
       ) : isError ? (
         <Flex
@@ -105,7 +142,7 @@ function Stats() {
           direction={{ base: "column", md: "row" }}
           m={4}
         >
-          <Alert w='50%' status='error'>
+          <Alert w="50%" status="error">
             <AlertIcon />
             Can not fetch data from server
           </Alert>
@@ -116,14 +153,26 @@ function Stats() {
           columns={{ base: 2, md: 4 }}
           spacing={{ base: 2, md: 10 }}
         >
-          <StatItem Label="Đơn mới hôm nay" Num={getData.totalTodayOrders} background="linear-gradient(135deg, #13072e, #3f2182)" />
-          <StatItem Label="Đã giao hôm nay" Num={getData.totalTodayDeliveries} background="linear-gradient(135deg, #ff2222, #ff6b00)" />
+          <StatItem
+            Label="Đơn mới hôm nay"
+            Num={getData.totalTodayOrders}
+            background="linear-gradient(135deg, #13072e, #3f2182)"
+          />
+          <StatItem
+            Label="Đã giao hôm nay"
+            Num={getData.totalTodayDeliveries}
+            background="linear-gradient(135deg, #ff2222, #ff6b00)"
+          />
           <StatItem
             Label="Doanh thu"
             Num={getData.totalRevenue}
             background="linear-gradient(135deg, #0086d1, #21fa93)"
           />
-          <StatItem Label="Đã nhận về" Num={getData.totalRevenue * 3 / 4} background="linear-gradient(90deg, #ff5e09, #ff0348)" />
+          <StatItem
+            Label="Đã nhận về"
+            Num={Math.round((getData.totalRevenue * 3) / 4 / 100) * 100}
+            background="linear-gradient(90deg, #ff5e09, #ff0348)"
+          />
         </SimpleGrid>
       )}
     </>
