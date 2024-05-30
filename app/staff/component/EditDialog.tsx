46c832331df87f82c46e797684cf2d56000985ab
HThanh-how <thanhlt1235@gmail.com>
2024-05-30 23:47:53 +0700

    Update lại css edit role

--- app/staff/component/EditDialog.tsx
+++ app/staff/component/EditDialog.tsx
@@ -16,27 +16,27 @@ import {
   useDisclosure,
   Textarea,
   useToast,
+  Flex,
 } from "@chakra-ui/react";
 
-
 import { ChangeEvent, useState, useEffect } from "react";
-import { useForm } from "react-hook-form"
-import { useEditEmployeePermissionMutation } from "@/app/_lib/features/api/apiSlice"
+import { useForm } from "react-hook-form";
+import { useEditEmployeePermissionMutation } from "@/app/_lib/features/api/apiSlice";
 import getFromLocalStorage from "@/app/_lib/getFromLocalStorage";
 
 type FormData = {
-  permissions: string[],
-  view: string,
-  create: string,
-  update: string,
-  manage: string,
-  create_product: string,
-  update_product: string,
-  create_store: string,
-  update_store: string,
-  create_receiver: string,
-  update_receiver: string,
-}
+  permissions: string[];
+  view: string;
+  create: string;
+  update: string;
+  manage: string;
+  create_product: string;
+  update_product: string;
+  create_store: string;
+  update_store: string;
+  create_receiver: string;
+  update_receiver: string;
+};
 
 export default function EditDialog({ isOpen, onClose, id }: any) {
   const toast = useToast();
@@ -45,50 +45,308 @@ export default function EditDialog({ isOpen, onClose, id }: any) {
     setValue,
     handleSubmit,
     reset,
+    watch,
     formState: { errors, isSubmitSuccessful, isSubmitting },
-  } = useForm<FormData>()
+  } = useForm<FormData>();
 
-  const [editEmployeePermission, {isLoading}] = useEditEmployeePermissionMutation();
+  const [editEmployeePermission, { isLoading }] =
+    useEditEmployeePermissionMutation();
 
   useEffect(() => {
-    if(isSubmitSuccessful) reset();
-  }, [isSubmitSuccessful, reset])
+    if (isSubmitSuccessful) reset();
+  }, [isSubmitSuccessful, reset]);
 
+  const viewOrder = watch("view");
+  const createOrder = watch("create");
+  const updateOrder = watch("update");
+  const manageOrder = watch("manage");
+
+  const [selectAllOrder, setSelectAllOrder] = useState(false);
+
+  useEffect(() => {
+    setSelectAllOrder(
+      viewOrder === "VIEW_ONLY" &&
+        createOrder === "CREATE_ORDER" &&
+        updateOrder === "UPDATE_ORDER" &&
+        manageOrder === "MANAGE_ORDER"
+    );
+  }, [viewOrder, createOrder, updateOrder, manageOrder]);
+  const OrderPermissions = ({ register }: { register: any }) => {
+    const handleSelectAllChange = () => {
+      const newSelectAllOrder = !selectAllOrder;
+      setSelectAllOrder(newSelectAllOrder);
+      if (newSelectAllOrder) {
+        setValue("view", "VIEW_ONLY");
+        setValue("create", "CREATE_ORDER");
+        setValue("update", "UPDATE_ORDER");
+        setValue("manage", "MANAGE_ORDER");
+      } else {
+        setValue("view", "");
+        setValue("create", "");
+        setValue("update", "");
+        setValue("manage", "");
+      }
+    };
+
+    return (
+      <Box m={4}>
+        <Flex>
+          <Text fontWeight={"bold"}>Đơn hàng</Text>
+          <Checkbox
+            ml={2}
+            isChecked={selectAllOrder}
+            colorScheme="green"
+            onChange={handleSelectAllChange}
+          />
+        </Flex>
+        <Flex justifyContent="space-between" mt={2} mb={4} ml={4}>
+          <Checkbox value="VIEW_ONLY" colorScheme="red" {...register("view")}>
+            Xem đơn
+          </Checkbox>
+          <Checkbox
+            value="CREATE_ORDER"
+            colorScheme="red"
+            {...register("create")}
+          >
+            Tạo đơn
+          </Checkbox>{" "}
+        </Flex>
+        <Flex justifyContent="space-between" mt={2} mb={4} ml={4}>
+          <Checkbox
+            value="UPDATE_ORDER"
+            colorScheme="red"
+            {...register("update")}
+          >
+            Cập nhật
+          </Checkbox>
+          <Checkbox
+            value="MANAGE_ORDER"
+            colorScheme="red"
+            {...register("manage")}
+            mr={1}
+          >
+            Quản lý
+          </Checkbox>
+        </Flex>
+      </Box>
+    );
+  };
+
+  const createProduct = watch("create_product");
+  const updateProduct = watch("update_product");
+
+  const [selectAllProduct, setSelectAllProduct] = useState(false);
+
+  useEffect(() => {
+    setSelectAllProduct(
+      createProduct === "CREATE_PRODUCT" && updateProduct === "UPDATE_PRODUCT"
+    );
+  }, [createProduct, updateProduct]);
+  const ProductPermissions = ({ register }: { register: any }) => {
+    const handleSelectAllChange = () => {
+      const newSelectAllProduct = !selectAllProduct;
+      setSelectAllProduct(newSelectAllProduct);
+      if (newSelectAllProduct) {
+        setValue("create_product", "CREATE_PRODUCT");
+        setValue("update_product", "UPDATE_PRODUCT");
+      } else {
+        setValue("create_product", "");
+        setValue("update_product", "");
+      }
+    };
+
+    return (
+      <Box m={4} mt={8}>
+        <Flex>
+          <Text fontWeight={"bold"}>Sản phẩm</Text>
+          <Checkbox
+            ml={2}
+            isChecked={selectAllProduct}
+            colorScheme="green"
+            onChange={handleSelectAllChange}
+          />
+        </Flex>
+        <Flex justifyContent="space-between" mt={2} mb={4} ml={4}>
+          <Checkbox
+            value="CREATE_PRODUCT"
+            colorScheme="red"
+            {...register("create_product")}
+          >
+            Tạo mới
+          </Checkbox>
+          <Checkbox
+            value="UPDATE_PRODUCT"
+            colorScheme="red"
+            {...register("update_product")}
+          >
+            Cập nhật
+          </Checkbox>
+        </Flex>
+      </Box>
+    );
+  };
+
+  const createStore = watch("create_store");
+  const updateStore = watch("update_store");
+
+  useEffect(() => {
+    setSelectAllStore(
+      createStore === "CREATE_STORE" && updateStore === "UPDATE_STORE"
+    );
+  }, [createStore, updateStore]);
+  const [selectAllStore, setSelectAllStore] = useState(false);
+  const StorePermissions = ({ register }: { register: any }) => {
+    const handleSelectAllChange = () => {
+      const newSelectAllStore = !selectAllStore;
+      setSelectAllStore(newSelectAllStore);
+      if (newSelectAllStore) {
+        setValue("create_store", "CREATE_STORE");
+        setValue("update_store", "UPDATE_STORE");
+      } else {
+        setValue("create_store", "");
+        setValue("update_store", "");
+      }
+    };
+
+    return (
+      <Box m={4} mt={8}>
+        <Flex>
+          <Text fontWeight={"bold"}>Cửa hàng</Text>
+          <Checkbox
+            ml={2}
+            isChecked={selectAllStore}
+            colorScheme="green"
+            onChange={handleSelectAllChange}
+          />
+        </Flex>
+        <Flex justifyContent="space-between" mt={2} mb={4} ml={4}>
+          <Checkbox
+            value="CREATE_STORE"
+            colorScheme="red"
+            {...register("create_store")}
+          >
+            Tạo mới
+          </Checkbox>
+          <Checkbox
+            value="UPDATE_STORE"
+            colorScheme="red"
+            {...register("update_store")}
+          >
+            Cập nhật
+          </Checkbox>
+        </Flex>
+      </Box>
+    );
+  };
+
+  const createReceiver = watch("create_receiver");
+  const updateReceiver = watch("update_receiver");
+
+  const [selectAllReceiver, setSelectAllReceiver] = useState(false);
+
+  useEffect(() => {
+    setSelectAllReceiver(
+      createReceiver === "CREATE_RECEIVER" &&
+        updateReceiver === "UPDATE_RECEIVER"
+    );
+  }, [createReceiver, updateReceiver]);
+
+  const ReceiverPermissions = ({ register }: { register: any }) => {
+    const handleSelectAllChange = () => {
+      const newSelectAllReceiver = !selectAllReceiver;
+      setSelectAllReceiver(newSelectAllReceiver);
+      if (newSelectAllReceiver) {
+        setValue("create_receiver", "CREATE_RECEIVER");
+        setValue("update_receiver", "UPDATE_RECEIVER");
+      } else {
+        setValue("create_receiver", "");
+        setValue("update_receiver", "");
+      }
+    };
+
+    return (
+      <Box m={4} mt={8}>
+        <Flex>
+          <Text fontWeight={"bold"}>Khách hàng</Text>
+          <Checkbox
+            ml={2}
+            isChecked={selectAllReceiver}
+            colorScheme="green"
+            onChange={handleSelectAllChange}
+          />
+        </Flex>
+        <Flex justifyContent="space-between" mt={2} mb={4} ml={4}>
+          <Checkbox
+            value="CREATE_RECEIVER"
+            colorScheme="red"
+            {...register("create_receiver")}
+          >
+            Thêm mới
+          </Checkbox>
+          <Checkbox
+            value="UPDATE_RECEIVER"
+            colorScheme="red"
+            {...register("update_receiver")}
+          >
+            Cập nhật
+          </Checkbox>
+        </Flex>
+      </Box>
+    );
+  };
   const onSubmit = async (data: FormData) => {
     data.permissions = [
-      data.view, data.create, data.update, data.manage, 
-      data.create_product, data.update_product, 
-      data.create_store, data.update_store,  
-      data.create_receiver, data.update_receiver
+      data.view,
+      data.create,
+      data.update,
+      data.manage,
+      data.create_product,
+      data.update_product,
+      data.create_store,
+      data.update_store,
+      data.create_receiver,
+      data.update_receiver,
     ];
-    data.permissions = data.permissions.filter((p) => typeof p === 'string');
-    const { view, create, update, manage, create_product, update_product, create_store, update_store, create_receiver, update_receiver, ...sendData } = data;
+    data.permissions = data.permissions.filter((p) => typeof p === "string");
+    const {
+      view,
+      create,
+      update,
+      manage,
+      create_product,
+      update_product,
+      create_store,
+      update_store,
+      create_receiver,
+      update_receiver,
+      ...sendData
+    } = data;
     let isSuccess: boolean = true;
     try {
-      await editEmployeePermission({newPermissions: sendData, id}).unwrap();
+      await editEmployeePermission({ newPermissions: sendData, id }).unwrap();
       onClose();
     } catch (err) {
       isSuccess = false;
-      console.error('Failed to edit permission: ', err)
+      console.error("Failed to edit permission: ", err);
       toast({
-        title: 'Có lỗi khi thực hiện cập nhật',
-        position: 'top',
-        status: 'error',
+        title: "Có lỗi khi thực hiện cập nhật",
+        position: "top",
+        status: "error",
         duration: 3000,
         isClosable: true,
-      })
+      });
     } finally {
-      if(isSuccess) {
+      if (isSuccess) {
         toast({
-          title: 'Cập nhật quyền thành công',
-          position: 'top',
-          status: 'success',
+          title: "Cập nhật quyền thành công",
+          position: "top",
+          status: "success",
           duration: 3000,
           isClosable: true,
-        })
+        });
       }
     }
-  }
+  };
 
   return (
     <>
@@ -103,82 +361,23 @@ export default function EditDialog({ isOpen, onClose, id }: any) {
                   }} onClick={onOpen}>
         Thêm nhân viên
       </Button> */}
-      
+
       <Modal
         closeOnOverlayClick={false}
         isOpen={isOpen}
         onClose={onClose}
         isCentered
-        size={{base: 'sm', md: 'md'}}
+        size={{ base: "sm", md: "md" }}
       >
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Chỉnh sửa quyền</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
-             <Box>
-            {/*  <Input my={4} placeholder={"Số điện thoại"} {...register('employeePhone', {
-                required: 'Trường này không được bỏ trống',
-                pattern: {
-                  value: /(03|07|08|09|01[2|6|8|9])+([0-9]{8})\b/,
-                  message: 'Số điện thoại không hợp lệ'
-                }
-                
-              })}/> */}
-              {/* {errors.employeePhone && <Text color="red.500" mb={2} mt={-2}>{errors.employeePhone.message}</Text>} */}
-              {/* <Input mt={4} placeholder={"Họ và tên"} /> */}
-              {/* Dropdown chọn thành phố */}
-              
-              {/* <Select
-                my={4}
-                placeholder="Chọn cửa hàng"
-                // value={selectedCity}
-                // onChange={handleCityChange}
-                variant="filled"
-              >
-                <option value="" disabled hidden>
-                  Chọn cửa hàng
-                </option>
-                
-                  <option value="1">Cửa hàng 1</option>
-                  <option value="2">Cửa hàng 2</option>
-                  <option value="3">Cửa hàng 3</option>
-                
-              </Select>   */}
-              <div className="flex gap-4">  
-                <Checkbox 
-                  defaultChecked
-                  colorScheme="red"
-                  value='VIEW_ONLY'
-                  {...register('view')}
-                >
-                  Xem đơn
-                </Checkbox>
-                <Checkbox 
-                  defaultChecked 
-                  value='CREATE_ORDER'
-                  {...register('create')}
-                  colorScheme="red"
-                >
-                  Tạo đơn
-                </Checkbox>
-
-                <Checkbox value='UPDATE_ORDER' colorScheme="red" {...register('update')}>Cập nhật</Checkbox>
-                <Checkbox value='MANAGE_ORDER' colorScheme="red" {...register('manage')}>Quản lý</Checkbox>
-              </div>
-              <div className="flex gap-4">  
-                <Checkbox value='CREATE_PRODUCT' colorScheme="red" {...register('create_product')}>Tạo sản phẩm mới</Checkbox>
-                <Checkbox value='UPDATE_PRODUCT' colorScheme="red" {...register('update_product')}>Cập nhật sản phẩm</Checkbox>
-              </div>
-              <div className="flex gap-4">  
-                <Checkbox value='CREATE_STORE' colorScheme="red" {...register('create_store')}>Tạo cửa hàng mới</Checkbox>
-                <Checkbox value='UPDATE_STORE' colorScheme="red" {...register('update_store')}>Cập nhật cửa hàng</Checkbox>
-              </div>  
-              <div className="flex gap-4">  
-                <Checkbox value='CREATE_RECEIVER' colorScheme="red" {...register('create_receiver')}>Thêm khách hàng mới</Checkbox>
-                <Checkbox value='UPDATE_RECEIVER' colorScheme="red" {...register('update_receiver')}>Cập nhật khách hàng</Checkbox>
-              </div>       
-            </Box>
+            <OrderPermissions register={register} />
+            <ProductPermissions register={register} />
+            <StorePermissions register={register} />
+            <ReceiverPermissions register={register} />
           </ModalBody>
 
           <ModalFooter>
@@ -188,31 +387,36 @@ export default function EditDialog({ isOpen, onClose, id }: any) {
             {isSubmitting ? (
               <Button
                 isLoading
-                loadingText='Đang gửi'
+                loadingText="Đang gửi"
                 color="white"
                 backgroundImage="linear-gradient(90deg, #ff5e09, #ff0348)"
                 sx={{
-                  '@media (hover: hover)': {
+                  "@media (hover: hover)": {
                     _hover: {
-                      backgroundImage: "linear-gradient(to right, #df5207, #d80740)"
-                    }
-                  }
+                      backgroundImage:
+                        "linear-gradient(to right, #df5207, #d80740)",
+                    },
+                  },
                 }}
               >
                 Cập nhật
               </Button>
             ) : (
-            <Button  color="white"
-                  backgroundImage="linear-gradient(90deg, #ff5e09, #ff0348)"
-                  sx={{
-                    '@media (hover: hover)': {
-                      _hover: {
-                        backgroundImage: "linear-gradient(to right, #df5207, #d80740)"
-                      }
-                    }
-                  }} onClick={handleSubmit(onSubmit)}>
-              Cập nhật
-            </Button>
+              <Button
+                color="white"
+                backgroundImage="linear-gradient(90deg, #ff5e09, #ff0348)"
+                sx={{
+                  "@media (hover: hover)": {
+                    _hover: {
+                      backgroundImage:
+                        "linear-gradient(to right, #df5207, #d80740)",
+                    },
+                  },
+                }}
+                onClick={handleSubmit(onSubmit)}
+              >
+                Cập nhật
+              </Button>
             )}
           </ModalFooter>
         </ModalContent>
